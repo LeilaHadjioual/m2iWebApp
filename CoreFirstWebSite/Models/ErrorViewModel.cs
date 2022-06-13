19261f2ec1dddb0fc611ddc9a54cbd74f361dc69
frup73152 <frup73152@ITMFR5CD127LH7N>
2022-06-13 12:02:47 +0200

    Ajoutez des fichiers projet.

---
+++ CoreFirstWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreFirstWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
